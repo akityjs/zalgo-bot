ec841f9f0e96f4acf16d4fcc0f5bb160ebe10b03
akityjs <84675799+akityjs@users.noreply.github.com>
2021-05-23 19:20:11 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+TOKEN = 'b12b0dd510e649ca0f3148c82ec32887b612fd6cceb91b6f8f29c117d3a4f3b5ed1a550878ef9840a9592'; // сюда вставляем свой токен
+// ID = id; // сюда вставляем ид паблика
+
+module.exports = {
+    TOKEN, // ID
+};
